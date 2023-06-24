72302483888d2f9ac28134e5aa342f35d6236c9f
Ali <alihassan.ah3535@gmail.com>
2023-06-24 12:42:49 +0500

    Sign up, Login Page
    Sign up, Login Page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import Counter from './features/counter/Counter';
 import './App.css';
 import Home from './pages/Home';
 import ProductList from './features/product-list/ProductList';
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage';
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      {/* <LoginPage></LoginPage> */}
+      <SignUpPage></SignUpPage>
       {/* <ProductList/> */}
     </div>
   );
