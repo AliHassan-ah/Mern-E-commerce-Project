4849aeb0973299090d76729b91bdc673f5ee55b3
Ali <alihassan.ah3535@gmail.com>
2023-06-25 10:05:36 +0500

    Checkout Page Completed

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -28,6 +28,23 @@ const products = [
   },
   // More products...
 ]
+const addresses = [
+ { name:'John Wick',
+  street:'11th Main',
+  city: 'Lahore',
+  pinCode :'110001',
+  state:'Punjab',
+  phone:'+92-31146646501'
+},
+ { name:'John Doe',
+  street:'15th Main',
+  city: 'Peshawar',
+  pinCode :5600,
+  state:'Kpk',
+  phone:'+92-31246646501'
+
+}
+]
 
 const Checkout = () => {
     const [open, setOpen] = useState(true)
@@ -36,10 +53,10 @@ const Checkout = () => {
      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
      <div className='grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5'>
         <div className='lg:col-span-3 py-5'>
-    <form className='bg-white px-5 py-2'>
+    <form className='bg-white px-5 py-2 mt-7'>
         <div className='space-y-12'>
      <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
+          <h2 className="text-2xl py-5 font-semibold leading-7 text-gray-900">Personal Information</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -167,12 +184,45 @@ const Checkout = () => {
             </div>
           </div>
         </div>
-
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          Reset
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Add Address
+        </button>
+      </div>
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Address</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             Choose from existing addresses
           </p>
+          <ul role="list" className="divide-y divide-gray-100">
+      {addresses.map((address) => (
+        <li key={address.email} className="flex justify-between gap-x-6 py-5 px-5 my-3 border-solid border-2 border-gray-200">
+          <div className="flex gap-x-4">
+                <input
+                    id="card"
+                    name="address"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                />
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">Name:{address.name}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">Street:{address.street}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">Pin Code : {address.pinCode}</p>
+            </div>
+          </div>
+          <div className="hidden sm:flex sm:flex-col sm:items-end">
+            <p className="text-sm leading-6 text-gray-900">Phone:{address.phone}</p>
+            <p className="text-sm leading-6 text-gray-900">City:{address.city}</p>
+            </div>
+        </li>
+      ))}
+    </ul>
 
           <div className="mt-10 space-y-10">
          
@@ -208,23 +258,11 @@ const Checkout = () => {
           </div>
         </div>
       </div>
-
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Save
-        </button>
-      </div>
     </form> 
     </div>
     <div className='lg:col-span-2'>
-    <div className="mx-auto mt-12 bg-white max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-   <h1 className="text-4xl font-bold tracking-tight text-gray-900 my-5">All Products</h1>
+    <div className="mx-auto mt-12 bg-white max-w-7xl px-0 py-5 sm:px-0 lg:px-0">
+   <h1 className="text-4xl font-bold tracking-tight text-gray-900 mb-1 px-1">Cart</h1>
        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                         <div className="flow-root">
                           <ul role="list" className="-my-6 divide-y divide-gray-200">
@@ -282,22 +320,22 @@ const Checkout = () => {
                       <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                       <div className="mt-6">
                         <Link
-                          to="/checkout"
+                          to="/pay"
                           className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                         >
-                          Checkout
+                          Pay & Order
                         </Link>
                       </div>
                       <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
                         <p>
-                          or
+                          or &nbsp;
                           <Link to='/'>
                           <button
                             type="button"
                             className="font-medium text-indigo-600 hover:text-indigo-500"
                             onClick={() => setOpen(false)}
                           >
-                            Continue Shopping
+                            Continue Shopping 
                             <span aria-hidden="true"> &rarr;</span>
                           </button></Link>
                      
